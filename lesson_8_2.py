e3e39df6a0b6b766f842f5985d81e54bb2a04d45
Baranova Ekaterina <recrane@rambler.ru>
2022-12-13 19:46:19 +1000

    change lesson_8_2

--- lesson_8_2.py
+++ lesson_8_2.py
@@ -1,38 +1,21 @@
 import requests
 
-class YaUploader:
+TOKEN = "  "  # ТОКЕН !!!!!
+base_host = 'https://cloud-api.yandex.net/'
+uri = 'v1/disk/resuorces/upload/'
+PREPARE_UPLOAD_URL = base_host + uri
+params = {'path': '/test_file.txt', 'overwrite': 'true'}
+headers = {'Accept': 'application/json', 'Authorization': TOKEN}
 
-    base_host = 'https://cloud-api.yandex.net'
+file_path = 'c:/Users/glesh/netology/super_heroes/test_file.txt'
 
-    def __init__(self, token):
-        self.token = token
+response = requests.get(PREPARE_UPLOAD_URL, params=params, headers=headers)
+print(response.text)
 
-    def get_headers(self):
-        return {
-            'Content-Type': 'application/json',
-            'Authorization': f'OAuth{self.token}'
-             }
+put_url = response.json().get('href')
+print(put_url)
 
-    def get_link(self, path):
-        uri = '/v1/disk/resuorces/upload'
-        request_url = self.base_host + uri
-        params = {'path': path, 'overwrite': True}
-        response = requests.get(request_url, headers=self.get_headers(), params=params)
-        print(response.json())
-        return response.json()['href']
+files = {'file': open(file_path, 'rb')}
+response = requests.put(put_url, files=files, headers=headers)
+print(response)
 
-    
-    def upload(self, file_path, yandex_path):
-        upload_url = self.get_link(yandex_path)
-        response = requests.put(upload_url, data=open(file_path, 'rb'), headers=self.get_headers())
-        if response.status_code == 201:
-            print('Загрузка прошла успешно')
-
-
-if __name__ == '__main__':
-    # Получить путь к загружаемому файлу и токен от пользователя
-    #yandex_path = '/test_file1.txt'
-    #file_path = '/test_file.txt'
-    token = ''
-    uploader = YaUploader(token)
-    result = uploader.upload('C:/Users/glesh/netology/super_heroes/test_file.txt', 'test_file1.txt')
